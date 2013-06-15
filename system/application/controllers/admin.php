118d82e4a2b1377811dd6208ebabb958b21598c2
Alphy L. Crampatanta <alph.ph@gmail.com>
2013-06-16 06:07:26 +0800

    + added admin controller

---
+++ system/application/controllers/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of admin
+ *
+ * @author alps
+ */
+class Admin extends CI_Controller {
+    //put your code here
+    function __construct() {
+        parent::__construct();
+    }
+    function index(){
+        echo 'test';
+    }
+}
+
+?>
