5621601ad4f64d320efc482573cd655605ab1481
USER <USER@USER-PC>
2013-06-19 11:40:54 +0800

    + DELETE table element

--- system/application/views/individual/admin_view.php
+++ system/application/views/individual/admin_view.php
@@ -17,7 +17,7 @@
                         <p>Welcome <?php echo anchor('/individual/profile/', ucwords($fname)); ?> <?php echo $date; ?></p>
                             <?php endforeach;?>
                         
-                        <table cellpadding="0" cellspacing="0" border="1" class="">
+                        <table>
                             <thead>
                                 <tr>
                                     <th>First</th>
@@ -35,15 +35,15 @@
         foreach (   
                 $info as $row
                 ) :?>                 
-           <tr style="text-align: right">
+           <tr>
                 <td width="10%"><?php echo ucwords($row->firstname);?></td>
                 <td width="10%"><?php echo ucwords($row->lastname);?></td>
                 <td width="10%"><?php echo $row->date_of_birth ?></td>
-                <td width="20%"><?php echo $row->drivers_license ?></td>
+                <td width="15%"><?php echo $row->drivers_license ?></td>
                 <td width="10%"><?php echo ucwords($row->state);?></td>
-                <td width="20%"><i><?php echo $row->email ?></i></td>
-                <td width="10%"><i><?php echo $row->status ?></i></td>                                    
-                <td width="10%"><?php echo $row->remarks ?></td>
+                <td width="15%"><?php echo $row->email ?></td>
+                <td width="10%"><?php echo $row->status ?></td>                                    
+                <td width="20%"><?php echo ucwords($row->remarks) ?></td>
             </tr>
  <?php
   endforeach;
